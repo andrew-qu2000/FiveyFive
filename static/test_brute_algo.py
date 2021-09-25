b5c1921a9f9b17398427ba5b1b9e91cda241449d
HermanLin <hl3123@nyu.edu>
2021-09-25 16:51:33 -0400

    template for future test file

--- static/test_brute_algo.py
+++ static/test_brute_algo.py
@@ -4,5 +4,6 @@ This is the test suite for brute_algo.py
 
 from unittest import TestCase
 
-from static.brute_algo import calc_team, calc_matchup_score
+import static.brute_algo
+#import static.fancy_algo
 
