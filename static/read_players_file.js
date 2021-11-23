a4f75aa55d14d7b4a63f50a9b877d1f925b491cc
andrew-qu2000 <aq447@nyu.edu>
2021-11-23 15:18:36 -0500

    delete old and unused files

--- static/read_players_file.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs')
-
-fs.readFile("./players.json", function(error, content) {
-  const obj = JSON.parse(content);
-  console.log(obj["0"].Name);
-})
--- static/test_brute_algo.py
+++
@@ -1,9 +0,0 @@
-"""
-This is the test suite for brute_algo.py
-"""
-
-from unittest import TestCase
-
-import static.brute_algo
-#import static.fancy_algo
-
