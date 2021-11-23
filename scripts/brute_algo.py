e84713a43aefdd29e30242a0c711e2b527a1d1f9
HermanLin <hl3213@nyu.edu>
2021-11-23 00:09:39 -0500

    initial file for brute force algorithm

---
+++ scripts/brute_algo.py
@@ -0,0 +1,16 @@
+"""
+Class for brute force approach to selecting the best team matchup.
+
+This approach guarantees that the global best matchup is selected.
+However, the runtime of the algorithm does increase as the size of 
+the teams increases. This is not ideal when trying to serve a best
+matchup to a client in a timely manner.
+"""
+
+class BruteAlgo():
+
+    def __init__(self):
+        pass
+
+    def matchup(squad):
+        pass
