420f538699f3a5b3bcb6ba3ca75f407def2d179d
andrew-qu2000 <aq447@nyu.edu>
2021-11-29 20:53:42 -0500

    comment out possible double redirect

--- app.py
+++ app.py
@@ -39,9 +39,9 @@ def run_dynamic_algo():
     best_matchup = DA.matchup()
     return json.dumps(best_matchup)
 
-@app.route('/static/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+# @app.route('/static/favicon.ico')
+# def favicon():
+#     return send_from_directory(os.path.join(app.root_path, 'static'),
+#                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 if __name__== '__main__':
     app.run('127.0.0.1', 5000, debug = True)
